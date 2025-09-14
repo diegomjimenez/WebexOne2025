0f7f9668b5f9ef59562eadf4d9738c6873c8a3d7
Diego M Jimenez <diego.jimenez.2007@gmail.com>
2025-09-14 22:44:47 +0200

    Update webex_bot.py

--- 02-bots/webex_bot.py
+++ 02-bots/webex_bot.py
@@ -1,15 +1,12 @@
 from dotenv import load_dotenv
 import os
+from webex_bot.commands.echo import EchoCommand
+from webex_bot.webex_bot import WebexBot
 
 # Load environment variables from .env file
 load_dotenv()
 
-from webex_bot.commands.echo import EchoCommand
-from webex_bot.webex_bot import WebexBot
-
 bot_token = os.getenv("BOT_TOKEN")
-name = os.getenv("NAME")
-email = os.getenv("EMAIL")
 
 # Create a Bot Object
 bot = WebexBot(teams_bot_token=bot_token,

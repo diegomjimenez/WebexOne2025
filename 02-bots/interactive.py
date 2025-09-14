ef2dd163dc8d48e69149293460ebd2f6e9383b4c
Diego M Jimenez <diego.jimenez.2007@gmail.com>
2025-09-14 21:58:51 +0200

    Update interactive.py

--- 02-bots/interactive.py
+++ 02-bots/interactive.py
@@ -1,1 +1,23 @@
+from dotenv import load_dotenv
+import os
 
+# Load environment variables from .env file
+load_dotenv()
+
+from webex_bot.commands.echo import EchoCommand
+from webex_bot.webex_bot import WebexBot
+
+bot_token = os.getenv("BOT_TOKEN")
+name = os.getenv("NAME")
+email = os.getenv("EMAIL")
+
+# Create a Bot Object
+bot = WebexBot(teams_bot_token=bot_token,
+               bot_name="WebexOne2025",
+               include_demo_commands=True)
+
+# Add new commands for the bot to listen out for.
+bot.add_command(EchoCommand())
+
+# Call `run` for the bot to wait for incoming messages.
+bot.run()

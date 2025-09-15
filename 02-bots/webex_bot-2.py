8cb1e2620a09b0b878e156613139da484ea50de2
Diego M Jimenez <diego.jimenez.2007@gmail.com>
2025-09-15 11:19:23 +0200

    Update webex_bot-2.py

--- 02-bots/webex_bot-2.py
+++ 02-bots/webex_bot-2.py
@@ -1,13 +1,15 @@
 from dotenv import load_dotenv
 import os
-from webex_bot.commands.echo import EchoCommand
 from webex_bot.webex_bot import WebexBot
+from webex_bot.models.command import Command
+from webexpythonsdk import WebexAPI
 
 # Load environment variables from .env file
 load_dotenv()
 
 bot_token = os.getenv("BOT_TOKEN")
 domain = os.getenv("DOMAIN")
+email = os.getenv("EMAIL")
 
 # Create a Bot Object
 bot = WebexBot(teams_bot_token=bot_token,
@@ -15,8 +17,30 @@ bot = WebexBot(teams_bot_token=bot_token,
                approved_domains=domain,
                include_demo_commands=False)
 
+class SendMessage(Command):
+
+    def __init__(self):
+        super().__init__(
+            command_keyword="message",
+            help_message="Send Message")
+
+    def execute(self, message, attachment_actions, activity):
+        """
+        :param message: message with command already stripped
+        :param attachment_actions: attachment_actions object
+        :param activity: activity object
+
+        :return: a string or Response object (or a list of either). Use Response if you want to return another card.
+        """
+        personid = attachment_actions.personId
+
+        webexbot = WebexAPI(bot_token)
+        webexbot.messages.create(toPersonId=personid, markdown="Hello!")
+
+        return "Message sent"
+
 # Include function and add it.
-## Plus remove unnecessary stuff
+bot.add_command(SendMessage())
 
 # Call `run` for the bot to wait for incoming messages.
 bot.run()
